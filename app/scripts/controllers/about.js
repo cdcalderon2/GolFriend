3e163a72bc56b0a1fc551d165845731a369cb8f7
carlos calderon <cdcalderon@gmail.com>
2014-11-09 07:39:48 -0600

    initial commit -- angular project generated using yeoman project generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name golfriendsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the golfriendsApp
+ */
+angular.module('golfriendsApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name golfriendsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the golfriendsApp
+ */
+angular.module('golfriendsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
